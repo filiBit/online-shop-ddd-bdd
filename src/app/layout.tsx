f5c9d759f1f04aea3002de2e59b8cdc2725dd387
Filip Biterski <filip.biterski@filibit.com>
2025-04-04 16:42:52 +0200

    feat: root-layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} flex w-[100%] flex-col items-center gap-16 p-[24px] pb-20 font-[family-name:var(--font-geist-sans)] antialiased sm:p-20`}
+            >
+                <header className="flex w-[100%] items-center border-b">
+                    <div className="bold px-[16px] text-xl">Store</div>
+                    <nav className="flex">
+                        <Link
+                            href="/"
+                            className={`h-[48px] content-center border border-b-0 bg-white px-[16px] hover:brightness-90`}
+                        >
+                            Home
+                        </Link>
+                    </nav>
+                </header>
+                <div className="w-[100%] max-w-[1400px]">{children}</div>
+            </body>
+        </html>
+    );
+}
